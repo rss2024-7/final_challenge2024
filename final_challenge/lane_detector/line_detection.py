8f434783195456534777b2170a28ac7a6b4a2c9e
jmiana <jmiana@mit.edu>
2024-05-05 22:20:47 -0400

    Update get_target_pix

--- final_challenge/lane_detector/line_detection.py
+++ final_challenge/lane_detector/line_detection.py
@@ -35,17 +35,19 @@ def get_target_pix(src):
     h, w, c = src.shape
 
     # Upper left pix of cropped img
-    ul_y = int(h/4)
+    ul_y = int(3*h/10)
     ul_x = int(w/6)
 
     # Bottom right pix of cropped img
-    br_y = 3*ul_y
+    br_y = round(.75*h)
     br_x = 5*ul_x
 
     edge_image = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
 
+    ret,thresh1 = cv.threshold(edge_image,150,255,cv.THRESH_BINARY)
+
     # limit field of view
-    crop = edge_image[ul_y:br_y, ul_x:br_x]
+    crop = thresh1[ul_y:br_y, ul_x:br_x]
 
     crop = cv.GaussianBlur(crop, (5, 5), 1)
 
@@ -67,7 +69,7 @@ def get_target_pix(src):
     cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
     cdstP = np.copy(cdst)
 
-    linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 200, 50)
+    linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 250, 50)
 
     slopes = []
 
@@ -76,39 +78,65 @@ def get_target_pix(src):
         return None
 
     # else
+    lines = linesP[:][0]
     for i in range(0, len(linesP)):
         l = linesP[i][0]
         p1 = (l[0], l[1])
         p2 = (l[2], l[3])
         slope = None
-        if abs(p2[1] - p1[1]) != 0:
+        y_dist = p2[1] - p1[1]
+        x_dist = p2[0] - p1[0]
+        if abs(x_dist) != 0:
             slope = (p2[1] - p1[1])/(p2[0]-p1[0])
         else:
             slope = np.inf
-        slopes.append([i, slope])
+        if abs(slope) >= .05 and slope < np.inf:
+            slopes.append([i, slope, abs(y_dist)])
     slopes = np.array(slopes)
     
     # sort slopes in ascending order: pos slope is right line, neg slope is left line
-    sorted = slopes[slopes[:, 1].argsort()]
+    if slopes.shape[0] != 0:
+        sorted = slopes[slopes[:, 1].argsort()]
+    else:
+        return None
 
     # height, width, channel info of cropped img
     hc, wc, cc = cdstP.shape
     
     # get two steepest lines
-    ## l2 is right line
+    right_lines = sorted[-4:]
+    right_lines = right_lines[right_lines[:, 2].argsort()]
+    left_lines = sorted[:4]
+    left_lines = left_lines[left_lines[:, 2].argsort()]
     l2 = linesP[int(sorted[-1][0])][0]
-    ## l1 is left line
     l1 = linesP[int(sorted[0][0])][0]
+
+    # if line with max y_dist is geq to lines with max slope and slope of line w/ max y_dist is within 20% of max slope
+    if right_lines[-1][2] >= sorted[-1][2] and abs((right_lines[-1][1]-sorted[-1][1])/sorted[-1][1]) <= 0.2:
+        l2 = linesP[int(right_lines[-1][0])][0]
+    if left_lines[-1][2] > sorted[0][2] and abs((left_lines[-1][1]-sorted[0][1])/sorted[0][1]) <= 0.2:
+        l1 = linesP[int(left_lines[-1][0])][0]
+
+
     avgx = (l1[2] + l2[0])//2
-    # avgy = (l1[3] + l2[1])//2
+    avgy = (l1[3] + l2[1])//2
     target_pix = (avgx + ul_x, hc//2 + ul_y)
+
 
     # did not detect right line
     if l2[3] <= l2[1]:
         l2 = None
         # use left lane's endpt and add offset to x
         target_pix = ( l1[2] + 30 + ul_x, l1[3] + ul_y)
+    
+    # did not detect left lane
+    if l1[1] <= l1[3]:
+        l1 = None
+        if l2 is None:
+            target_pix = (wc//2 + ul_x, hc//2 + ul_y)
+        else:
+            target_pix = ( l2[0] - 30 + ul_x, l2[1] + ul_y)
 
     # further modification: add intersection check? or will that be too far?
 
-    return target_pix
+    return target_pix, [l1, l2], [ul_x, ul_y]
